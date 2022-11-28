02a18f8e521545c763c9105c04a180b503d77af8
Chiko Tridipa <chiko.tridipac@gmail.com>
2022-11-28 14:28:46 +0700

    Fix NumberPosIndex & Add Divide Function with Rounding

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,67 +1,55 @@
-import React, { useContext, useState } from "react";
-import { appContext } from "../App";
+import React, { useContext, useState } from 'react'
+import { appContext } from '../App'
 
-function Key({ keyVal, bigKey, ope }) {
-  const { board, setBoard, currAttempt, setCurrAttempt } =
-    useContext(appContext);
+function Key({keyVal, bigKey, ope}) {
+  const { board, setBoard, currAttempt, setCurrAttempt } = useContext(appContext);
   const pos = currAttempt.numberPos;
   let decision;
 
-  if (ope == "number") {
-    pos == 1 ? (decision = false) : (decision = true);
-  } else if (ope == "operator") {
-    pos == 1 ? (decision = true) : (decision = false);
+  if( ope == 'number'){
+    pos == 1 ? decision = false : decision = true;
+  } else if ( ope == 'operator'){
+    pos == 1 ? decision = true : decision = false;
   } else {
     decision = true;
   }
 
+
   const selectLetter = () => {
-    if (keyVal === "ENTER") {
-      if (currAttempt.numberPos !== 3) return;
-      const newBoard = { ...board };
-      if (board[currAttempt.attempt][1] === "+") {
-        newBoard[currAttempt.attempt][3] =
-          parseInt(board[currAttempt.attempt][0]) +
-          parseInt(board[currAttempt.attempt][2]);
-      } else if (board[currAttempt.attempt][1] === "-") {
-        newBoard[currAttempt.attempt][3] =
-          parseInt(board[currAttempt.attempt][0]) -
-          parseInt(board[currAttempt.attempt][2]);
-      } else if (board[currAttempt.attempt][1] === "*") {
-        newBoard[currAttempt.attempt][3] =
-          parseInt(board[currAttempt.attempt][0]) *
-          parseInt(board[currAttempt.attempt][2]);
-      } else if (board[currAttempt.attempt][1] === "/") {
-        newBoard[currAttempt.attempt][3] = Math.round(
-          parseInt(board[currAttempt.attempt][0]) /
-            parseInt(board[currAttempt.attempt][2])
-        );
+
+    if(keyVal === "ENTER"){
+      if(currAttempt.numberPos !== 3) return;
+      const newBoard = {...board};
+      if(board[currAttempt.attempt][1] === '+'){
+        newBoard[currAttempt.attempt][3] = parseInt(board[currAttempt.attempt][0]) + parseInt(board[currAttempt.attempt][2]);
+      } else if(board[currAttempt.attempt][1] === '-'){
+        newBoard[currAttempt.attempt][3] = parseInt(board[currAttempt.attempt][0]) - parseInt(board[currAttempt.attempt][2]);
+      } else if(board[currAttempt.attempt][1] === '*'){
+        newBoard[currAttempt.attempt][3] = parseInt(board[currAttempt.attempt][0]) * parseInt(board[currAttempt.attempt][2]);
+      } else if(board[currAttempt.attempt][1] === '/'){
+        newBoard[currAttempt.attempt][3] = Math.round(parseInt(board[currAttempt.attempt][0]) / parseInt(board[currAttempt.attempt][2]));
       }
       setBoard(newBoard);
-      setCurrAttempt({ attempt: currAttempt.attempt + 1, numberPos: 0 });
-    } else if (keyVal === "DELETE") {
-      if (currAttempt.numberPos === 0) return;
-      const newBoard = { ...board };
+      setCurrAttempt({attempt: currAttempt.attempt + 1, numberPos: 0});
+    } else if(keyVal === "DELETE") {
+      if(currAttempt.numberPos === 0) return;
+      const newBoard = {...board};
       newBoard[currAttempt.attempt][currAttempt.numberPos - 1] = "";
-      setBoard(newBoard);
-      setCurrAttempt({ ...currAttempt, numberPos: currAttempt.numberPos - 1 });
+      setBoard(newBoard)
+      setCurrAttempt({...currAttempt, numberPos: currAttempt.numberPos - 1});
     } else {
-      if (currAttempt.numberPos > 2) return;
-      const newBoard = { ...board };
+      if(currAttempt.numberPos > 2) return;
+      const newBoard = {...board};
       newBoard[currAttempt.attempt][currAttempt.numberPos] = keyVal;
       setBoard(newBoard);
-      setCurrAttempt({ ...currAttempt, numberPos: currAttempt.numberPos + 1 });
+      setCurrAttempt({...currAttempt, numberPos: currAttempt.numberPos + 1});
     }
-  };
+    
+    
+  }
   return (
-    <div
-      className="key"
-      id={bigKey && "big"}
-      onClick={decision == true ? selectLetter : null}
-    >
-      {keyVal}
-    </div>
-  );
+    <div className='key' id = {bigKey && "big"} onClick={ decision == true ? selectLetter : null }>{keyVal}</div>
+  )
 }
 
-export default Key;
+export default Key

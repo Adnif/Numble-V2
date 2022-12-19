ddb72c242808e3a7d78d1c53b541f11bd57a0abc
Adnif <ahmadbani9@gmail.com>
2022-12-19 15:45:16 +0700

    prepare deploy

--- server/index.js
+++ server/index.js
@@ -21,6 +21,19 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/", gameRoutes);
 app.use("/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
